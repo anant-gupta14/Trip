c717a78d109cb18e2b944d9084ab54386b444e5c
Anant Gupta <cs.anantgupta@gmail.com>
2018-07-16 22:16:35 +0530

    fix for added duplicate users and phone number in click setting

--- app/src/main/java/com/dev/infinitoz/trip/AdminManagementActivity.java
+++ app/src/main/java/com/dev/infinitoz/trip/AdminManagementActivity.java
@@ -29,10 +29,11 @@ public class AdminManagementActivity extends AppCompatActivity {
     String tripId;
     //FirebaseRecyclerAdapter<User, UserViewHolder> adapter;
     UserViewAdapter adapter;
-    List<User> users = new ArrayList<>();
+
     private RecyclerView recyclerView;
     private DatabaseReference tripUserDBReference, userDBRef;
     private boolean isUserView;
+    private List<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,39 +56,46 @@ public class AdminManagementActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    users = new ArrayList<>();
                     Map<String, Object> usersMap = (Map<String, Object>) dataSnapshot.getValue();
                     final Integer[] count = new Integer[1];
                     count[0] = 0;
                     for (String userID : usersMap.keySet()) {
                         count[0] = new Integer(count[0].intValue() + 1);
+
                         Map<String, Object> userData = (Map<String, Object>) usersMap.get(userID);
                         if (userData.get(Constants.IS_REMOVED) == null || !(boolean) userData.get(Constants.IS_REMOVED)) {
-                            FirebaseDatabase.getInstance().getReference(Constants.USERS).child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
-                                        User user = dataSnapshot.getValue(User.class);
-                                        user.setuId(userID);
-                                        Log.d("User", user.toString());
-                                        users.add(user);
+                            User user = checkUserInMap(userID);
+                            if (user != null) {
+                                users.add(user);
+                            } else {
+                                FirebaseDatabase.getInstance().getReference(Constants.USERS).child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                                            User user = dataSnapshot.getValue(User.class);
+                                            user.setuId(userID);
+                                            Log.d("User", user.toString());
+                                            ((Map<String, User>) TripContext.getValue(Constants.USER_DATA_MAP)).put(userID, user);
+                                            users.add(user);
 
-                                    }
-                                    if (count[0].equals(usersMap.size())) {
-                                        if (isUserView) {
-                                            adapter = new UserViewOnlyAdapter(users);
-                                            recyclerView.setAdapter(adapter);
-                                        } else {
+                                        }
+                                        if (count[0].equals(usersMap.size())) {
                                             adapter = new UserViewAdapter(users);
                                             recyclerView.setAdapter(adapter);
                                         }
                                     }
-                                }
 
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
 
-                                }
-                            });
+                                    }
+                                });
+                            }
+                            if (count[0].equals(usersMap.size())) {
+                                adapter = new UserViewAdapter(users);
+                                recyclerView.setAdapter(adapter);
+                            }
                         }
                     }
                 }
@@ -100,52 +108,14 @@ public class AdminManagementActivity extends AppCompatActivity {
             }
         });
 
-
-
-        /*
-        userDBRef = FirebaseDatabase.getInstance().getReference(Constants.USERS);
-        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
-                .setIndexedQuery( qry,userDBRef, User.class)
-                .build();
-
-        adapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull User user) {
-                holder.userName.setText(user.getName());
-            }
-
-            @NonNull
-            @Override
-            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.field, parent, false);
-
-                return new UserViewHolder(view);
-            }
-        };
-
     }
 
-    public void onDelete(View v) {
-       // parentLinearLayout.removeView((View) v.getParent());
-    }
-
-    public void onAddField(String userName) {
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View rowView = inflater.inflate(R.layout.field, null);
-        TextView userTV = rowView.findViewById(R.id.userName);
-        userTV.setText(userName);
-        // Add the new row before the add field button.
-        //parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);
-    }
-
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }*/
+    private User checkUserInMap(String userID) {
+        Map<String, User> userMap = (Map<String, User>) TripContext.getValue(Constants.USER_DATA_MAP);
+        if (userMap != null) {
+            return userMap.get(userID);
+        }
+        return null;
     }
 
     public void onRemove(View view) {
@@ -187,8 +157,12 @@ public class AdminManagementActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
             holder.userName.setText(users.get(position).getName());
+            holder.userPhone.setText(users.get(position).getPhone());
             holder.deleteBtn.setTag(position);
             Log.d("position::", +position + "");
+            if (TripContext.getValue(Constants.IS_USER_VIEW) != null) {
+                holder.deleteBtn.setVisibility(View.GONE);
+            }
         }
 
         @Override
@@ -200,55 +174,15 @@ public class AdminManagementActivity extends AppCompatActivity {
     }
 
     public class UserViewHolder extends RecyclerView.ViewHolder {
-        TextView userName;
+        TextView userName, userPhone;
         Button deleteBtn;
 
         public UserViewHolder(View itemView) {
             super(itemView);
             userName = itemView.findViewById(R.id.userName);
+            userPhone = itemView.findViewById(R.id.userPhone);
             deleteBtn = itemView.findViewById(R.id.delete_button);
         }
     }
 
-    public class UserViewOnlyAdapter extends UserViewAdapter {
-        List<User> users;
-
-
-        public UserViewOnlyAdapter(List<User> users) {
-            this.users = users;
-        }
-
-        @NonNull
-        @Override
-        public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.field, parent, false);
-
-            return new UserViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-            holder.userName.setText(users.get(position).getName());
-        }
-
-        @Override
-        public int getItemCount() {
-            return users.size();
-        }
-
-
-    }
-
-    public class UserViewOnlyHolder extends RecyclerView.ViewHolder {
-        TextView userName;
-        Button deleteBtn;
-
-        public UserViewOnlyHolder(View itemView) {
-            super(itemView);
-            userName = itemView.findViewById(R.id.userName);
-            deleteBtn = itemView.findViewById(R.id.delete_button);
-            deleteBtn.setVisibility(View.GONE);
-        }
-    }
 }
